5e61495cce39dd36ef62ca23d602979899087800
Your Name <you@example.com>
2021-07-26 18:15:25 +0900

    detail page

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 from data import Articles
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     articles = Articles()
-    return render_template('index.html', articles=articles )
-    
+    # print(articles)
 
-if __name__ == '__main__':
+    for  i in articles:
+        print(i['title'])
+    return render_template('index.html' , articles =articles )
+
+@app.route('/<id>/article', methods=['GET', 'POST'])
+def detail(id):
+    if request.method == 'GET':
+        articles = Articles()
+        print(articles[int(id)-1])
+        return render_template('detail.html' , article=articles[int(id)-1] )
+
+if __name__ == '__main__': 
     app.run() 
